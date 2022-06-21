1d5228d2dc1253f22e0c299a61eac71813e4d651
Kasim <kasimplecoder@gmai.com>
2022-06-21 21:37:47 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Basic Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
@@ -26,6 +26,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text("hi");
   }
 }
