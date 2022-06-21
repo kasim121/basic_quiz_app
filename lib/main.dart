fbfea2d061f69f04d49701da0a5288c758176379
Kasim <kasimplecoder@gmai.com>
2022-06-21 21:58:08 +0530

    demo-mergin-changes-done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() =>
   runApp(const MyApp());
-
+//demo merging
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
